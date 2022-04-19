9ee8a72be2f0c5976f41c39756a825721d7f37e3
andrei-lastochkin-idf <andrei.lastochkin@idfinance.com>
2022-04-19 15:03:28 +0300

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:custom_widget/circle_highlighted_diagram.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(
+        home: HomePage(),
+      );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Color(0xFF92A3AA),
+        body: Center(
+          child: Container(),
+        ),
+      );
+}
